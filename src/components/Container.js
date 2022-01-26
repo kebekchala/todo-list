218d3dd4b371aa9c5e1b3e7f15556778cbd88310
kebekchala <chalakebek@gmail.com>
2022-01-26 16:43:14 +0300

    Updates

--- src/components/Container.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Container = ({ children }) => {
-    return <section className='container'>{children}</section>;
-};
-
-export default Container;
--- src/components/Input.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Input = ({ placeholder }) => {
-  return ;
-}
-
-export default Input;
